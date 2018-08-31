9957ecdc6a9138cd3fa830e3a7d1972b06d1d738
Vladislav <vlad.donskikh@gmail.com>
2018-09-01 02:41:09 +0300

    popup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,4 +1,24 @@
-$(document).ready(() => {
-  console.log('test popup');
+$(document).ready(function () {
+  chrome.storage.sync.get('films', function (items) {
+    for (let i = 0; i < items.films.films.length; i++) {
+      let div = document.createElement("div");
+      div.className = "item" + i + " hider";
+      div.textContent = items.films.films[i].title;
+      $(".container").append(div);
+      for (let j = 0; j < items.films.films[i].cinema.length; j++) {
+        let div = document.createElement("div");
+        div.className = "ci" + i + j + " hidden";
+        div.textContent = items.films.films[i].cinema[j].name;
+        $(".item" + i).append(div);
+      }
+    }
+  });
+  // $(".hider").click(function () {
+  //   $(".hidden").toggle("slow");
+  //   console.log('123');
+  // });
+  $(document).on('click', '.hider', function () {
+    //$(".hidden").toggle("slow");
+    $(this).children().toggle("slow");
+  });
 });
-
